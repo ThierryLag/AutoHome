4c32ac59feb80b1b3c4fb2daad131256d14b9ad5
Thierry Lagasse <epagneul@dogstudio.be>
2016-08-28 23:16:27 +0200

    Create Dimmer plugin

---
+++ src/Autohome/Plugins/Niko/DimmerPlugin.php
@@ -0,0 +1,30 @@
+<?php
+namespace Autohome\Plugins\Niko;
+use Autohome\Plugins\Niko\Exceptions\NikoPluginException;
+
+class DimmerPlugin extends NikoPlugin
+{
+    public function execute($action)
+    {
+        if(!isset($action['id'])) {
+            throw new NikoPluginException('Action dimmer must have an ID');
+        }
+
+        $id = $action['id'];
+        $value = isset($action['value']) ? $action['value'] : 0;
+
+        if(isset($action['range']) && $action['range']) {
+            $value = max(1 , min(100, floor(100 * $action['delta'] / $action['range'])));
+        }
+
+        /**
+         * Value :
+         *  0-100 : set the value
+         *  254 : recall the last value
+         *  255 : shutdown with overwrite the value
+         */
+
+        $response = $this->sendCommand('executeactions', ['id' => $id, 'value1' => $value]);
+        return ($response && $response['error'] == 0) ? $value : false;
+    }
+}
