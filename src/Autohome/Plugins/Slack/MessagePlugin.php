0c5832d7dc5c59c925455edbe5c67d01095cea1e
Thierry Lagasse <epagneul@dogstudio.be>
2016-08-29 08:43:36 +0200

    Add variable parsing for Slack message

--- src/Autohome/Plugins/Slack/MessagePlugin.php
+++ src/Autohome/Plugins/Slack/MessagePlugin.php
@@ -29,7 +29,10 @@ class MessagePlugin implements PluginInterface
             isset($action['to']) && $message->to($action['to']);
             isset($action['icon']) && $message->setIcon($action['icon']);
 
-            $message->send($action['message']);
+            $message->send(preg_replace_callback('/\{.*\}/', function($matches) use ($action) {
+                $keys = substr($matches[0], 1, -1);
+                return isset($action[$keys]) ? $action[$keys] : '___';
+            }, $action['message']));
         }
         return true;
     }
