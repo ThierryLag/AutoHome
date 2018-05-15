c8439c42ece7002e55be99bfbde6ee3a725d05f3
Thierry Lagasse <epagneul@dogstudio.be>
2018-05-15 07:50:10 +0200

    Add the current time when test.

--- cli/test.php
+++ cli/test.php
@@ -5,6 +5,8 @@ include_once __DIR__ . '/../vendor/autoload.php';
 use \Autohome\Timeline;
 $timeline = Timeline::load(__DIR__ . '/../config/app.yml');
 
+echo "Now, it's ", (new \DateTime())->format('H:i'), PHP_EOL;
+
 for ($hour=0; $hour < 24; $hour++) {
     for ($minute=0; $minute < 60; $minute++) {
         $fakeNow = (new \DateTime())->setTime($hour,  $minute);
