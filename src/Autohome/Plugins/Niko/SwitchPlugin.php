fbf84d90e0d15a60f185c77e4dd822e83ee65a8b
Thierry Lagasse <epagneul@dogstudio.be>
2018-12-30 14:30:24 +0100

    Add 'open/close' option for Nikoswitch (used as Shutter)

--- src/Autohome/Plugins/Niko/SwitchPlugin.php
+++ src/Autohome/Plugins/Niko/SwitchPlugin.php
@@ -4,7 +4,7 @@ namespace Autohome\Plugins\Niko;
 class SwitchPlugin extends NikoPlugin
 {
     protected $values = [
-        'on' => 1,  '254' => 1,
-        'off' => 0, '255' => 0,
+        'on' => 1, 'open' => 1, '254' => 1,
+        'off' => 0, 'close' => 0, '255' => 0,
     ];
 }
